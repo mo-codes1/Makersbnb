d3bee5fb7412707f7f3d4e08d31bcdc50cac1fb0
Courtney Stow <courtney@Courtneys-MacBook-Air.local>
2021-11-16 14:28:07 +0000

    Changed room_spec.rb feature test room 1 to Windsor Castle. Also changed the name of spaces.rb to space.rb

--- spec/features/room_spec.rb
+++ spec/features/room_spec.rb
@@ -1,6 +1,6 @@
 feature 'viewing a room' do
   scenario 'user will be able to view a room on the site' do
     visit '/spaces'
-    expect(page).to have_content('Room 1')
+    expect(page).to have_content('Windsor Castle')
   end
 end
