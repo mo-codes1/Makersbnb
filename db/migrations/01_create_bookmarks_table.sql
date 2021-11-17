4cf31f2c0d3c0acb59ac8d0643a30db4e34c2185
Kate Young <89363807+katemyoung@users.noreply.github.com>
2021-11-17 09:20:18 +0000

    Add reset_test_database helper method

--- db/migrations/01_create_bookmarks_table.sql
+++ db/migrations/01_create_bookmarks_table.sql
@@ -1,3 +1,1 @@
 CREATE TABLE spaces(id SERIAL PRIMARY KEY, name VARCHAR(60), available BOOL);
-INSERT INTO spaces (name, available) VALUES ('Windsor Castle', true);
-INSERT INTO spaces (name, available) VALUES ('Roka Rooms', true);
