f32b670aec068a310645ecdbc5c4dd23abfd031d
Mohammed Salah <mohammed_salah@icloud.com>
2021-11-17 12:51:43 +0000

    Fixed buttons for request

--- spec/space_spec.rb
+++ spec/space_spec.rb
@@ -10,4 +10,13 @@ describe Space do
       expect(spaces).to include('Roka Rooms')
     end
   end
+
+  describe '#book' do
+    it 'should set availability of spaces to false' do
+      conn = PG.connect(dbname: 'makersbnb_test')
+      spaces = Space.update
+      expect('Windsor Castle').to updated_with(false)
+    end    
+  end
+
 end
