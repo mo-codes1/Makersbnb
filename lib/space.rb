74f5627e966567513b95ffa95df28a514cfada79
Kate Young <89363807+katemyoung@users.noreply.github.com>
2021-11-18 10:47:37 +0000

    Add default value available=true when listing a new space

--- lib/space.rb
+++ lib/space.rb
@@ -14,12 +14,12 @@ class Space
   end
 
 
-  def self.create(space)
+  def self.create(name:) # add function: set available to true by default
     if ENV['ENVIRONMENT'] == 'test'
       connection = PG.connect(dbname: "makersbnb_test")
     else
       connection = PG.connect(dbname: "makersbnb")
     end
-      connection.exec("INSERT INTO spaces (name) VALUES('#{space}');")
+      connection.exec("INSERT INTO spaces (name, available) VALUES('#{name}', true) RETURNING id, name, available") 
   end
 end
--- spec/space_spec.rb
+++ spec/space_spec.rb
@@ -12,11 +12,20 @@ describe Space do
   end
 
   describe '#book' do
+    
     it 'should set availability of spaces to false' do
+      pending
       conn = PG.connect(dbname: 'makersbnb_test')
       spaces = Space.update
-      expect('Windsor Castle').to updated_with(false)
+      expect('Windsor Castle').to updated_with(false) # this is sudo code
     end    
   end
+  
+  describe '.create' do 
+    it 'creates a new space' do
+    space = Space.create(name: 'The Hilton').first #what does .first do here?
+    expect(space['name']).to eq 'The Hilton'
+    end
+  end 
 
 end
