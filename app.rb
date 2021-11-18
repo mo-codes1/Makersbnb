39e43bf37236c1024c39d73c5f07017b44259250
Courtney Stow <courtney@Courtneys-MacBook-Air.local>
2021-11-18 12:11:45 +0000

    Updated functionality on spaces.rb. Created new page list_space.

--- app.rb
+++ app.rb
@@ -1,6 +1,7 @@
 require 'sinatra/base'
 require 'sinatra/reloader'
 require './lib/space'
+require './lib/owner'
 
 class BnB < Sinatra::Base
   configure :development do
@@ -22,6 +23,14 @@ class BnB < Sinatra::Base
 
   post '/spaces' do
     Space.create(params[:space])
+    redirect '/spaces'
+  end
+
+  get '/add_space' do
+    erb(:list_a_space)
+  end
+
+  post '/add_space' do
     redirect '/spaces'
   end
 
