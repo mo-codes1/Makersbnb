146e43599e5530d1a6de57352d990a23e2570c93
Jacob <jacobchanrahan@gmail.com>
2021-11-18 15:03:49 +0000

    updating booking function

--- spec/features/booked_spec.rb
+++ spec/features/booked_spec.rb
@@ -4,3 +4,14 @@ feature "booking a room" do
         expect(page).to have_content "Room Requested"
     end
 end
+
+feature "Updating available status" do
+    scenario "A guest can book a room" do
+        space = Space.create(name: "Tudor house")
+        visit('/spaces')
+        expect(page).to have_content "Tudor house"
+        within_fieldset("Tudor house") do
+        click_button "Request booking"
+        end
+    end
+end
--- spec/space_spec.rb
+++ spec/space_spec.rb
@@ -16,13 +16,13 @@ describe Space do
     end
   end
 
-  describe '#book' do
-    
+  describe '.book' do
     it 'should set availability of spaces to false' do
-      pending
-      conn = PG.connect(dbname: 'makersbnb_test')
-      spaces = Space.update
-      expect('Windsor Castle').to updated_with(false) # this is sudo code
+      space = Space.create(name: 'The Hilton')
+      persisted_data = PG.connect(dbname: 'makersbnb_test').query("SELECT * FROM spaces WHERE id = #{space.id};")
+      booked_space = Space.book(id: space.id)
+      expect(space.name).to eq 'The Hilton'
+      expect(space.available).to eq 'f'
     end    
   end
   
