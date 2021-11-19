040cf4c8ca74ac26a66ecda371d3d14b16bd7489
Kate Young <89363807+katemyoung@users.noreply.github.com>
2021-11-19 15:54:03 +0000

    Update tests

--- spec/features/booked_spec.rb
+++ spec/features/booked_spec.rb
@@ -7,7 +7,7 @@ end
 
 feature "Updating available status" do
     scenario "A guest can book a room" do
-        space = Space.create(name: "Windsor Castle")
+        space = Space.create(name: "Windsor Castle", owner_name: "Jeff", price: "100", description: "Spacious")
         visit('/spaces')
         expect(page).to have_content "Windsor Castle"
         within_fieldset("Windsor Castle") do
